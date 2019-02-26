ccdd92aae27ebc8d0a4d788bf1d6e4e2299d1507
rsqwa <rsqwa@DESKTOP-1O7K6EF>
2019-02-25 17:24:42 -0700

    Initial Version with JavaDocs

---
+++ CSE360assgn2/src/cse360assgn2/Calculator.java
@@ -0,0 +1,40 @@
+/**	Rida Shiban
+ *	ASU ID: 1212541783
+ *	CSE 360 Assgn 2
+ *	Description: 
+ */
+
+package cse360assgn2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
