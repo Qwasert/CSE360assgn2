e2d9bb15df5d8040f98c9d62531c0fc932550cab
rsqwa <rsqwa@DESKTOP-1O7K6EF>
2019-02-25 17:52:17 -0700

    Third version final

--- CSE360assgn2/src/cse360assgn2/Calculator.java
+++ CSE360assgn2/src/cse360assgn2/Calculator.java
@@ -1,7 +1,7 @@
 /**	Rida Shiban
  *	ASU ID: 1212541783
  *	CSE 360 Assgn 2
- *	Description: Scientific calculator
+ *	Description: Scientific calculator that carries a history
  */
 
 package cse360assgn2;
@@ -9,10 +9,10 @@ package cse360assgn2;
 public class Calculator {
 
 	private int total;
+	private String history = "0"; //keeps track of all method calls
 	
 	public Calculator () {
 		total = 0; // not needed - included for clarity
-		
 	}
 	
 	/**
@@ -24,30 +24,37 @@ public class Calculator {
 	
 	/**
 	 * Takes an integer and adds it to the calculator's total
+	 * This method is recorded in the history string
 	 * @param value is user input
 	 */
 	public void add (int value) {
 		total = total + value;
+		history += " + " + value;
 	}
 
 	/**
 	 * Takes an integer and subtracts it from the calculator's total
+	 * This method is recorded in the history string
 	 * @param value is user input
 	 */
 	public void subtract (int value) {
 		total = total - value;
+		history += " - " + value;
 	}
 
 	/**
 	 * Takes an integer and multiplies it by the calculator's total
+	 * This method is recorded in the history string
 	 * @param value is user input
 	 */
 	public void multiply (int value) {
 		total = total * value;
+		history += " * " + value;
 	}
 
 	/**
 	 * Takes an integer and divides it by the calculator's total
+	 * This method is recorded in the history string
 	 * @param value is user input, if input is 0, the method returns
 	 *  	  0 and does not divide
 	 */
@@ -56,9 +63,13 @@ public class Calculator {
 			total = 0;
 		else
 			total = total / value;
+		history += " / " + value;
 	}
 	
+	/**
+	 * @return the history of all computations
+	 */
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
